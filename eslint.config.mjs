0a7662326712271b1399e62eaa919f860de77138
Adam Cegiełka <adamcegielka@tlen.pl>
2025-04-20 13:33:49 +0200

    Fix: Resolve ESLint issues and update linting configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,33 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs,ts,tsx}'],
+    ignores: ['node_modules/', 'tips/', 'utils/', 'README.md'], // Add ignored paths here
+    languageOptions: {
+      parser: tsParser,
+      globals: {
+        ...globals.browser,
+        process: true,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      '@typescript-eslint/no-unused-expressions': [
+        'error',
+        {
+          allowShortCircuit: true,
+          allowTernary: true,
+          allowTaggedTemplates: true,
+        },
+      ],
+    },
+  },
 ];
