eeb61b076984df2ed4ba418d1ca92f5c194cfc37
Adam Cegiełka <adamcegielka@tlen.pl>
2025-05-03 14:34:15 +0200

    refactor test API

---
+++ tests/api_ReqRes/1_GET_users.spec.ts
@@ -0,0 +1,180 @@
+import { test, expect } from '@playwright/test';
+
+interface User {
+  id: number;
+  email: string;
+  first_name: string;
+  last_name: string;
+  avatar: string;
+}
+
+test.describe.parallel('Get users', () => {
+  const baseUrl = 'https://reqres.in/api';
+
+  test('Assert Response for user from page 1', async ({ request }) => {
+    const response = await request.get(`${baseUrl}/users`, {
+      params: { page: 1 },
+    });
+
+    // Assert
+    expect(response.status()).toBe(200);
+    console.log('Status:', response.status());
+
+    // Response body
+    const responseBody = JSON.parse(await response.text());
+    console.log(responseBody);
+
+    // Assert structure
+    expect(responseBody).toHaveProperty('page');
+    expect(responseBody).toHaveProperty('per_page');
+    expect(responseBody).toHaveProperty('total');
+    expect(responseBody).toHaveProperty('total_pages');
+    expect(responseBody).toHaveProperty('data');
+
+    // Assert values
+    expect(responseBody.page).toBe(1);
+    expect(responseBody.per_page).toBe(6);
+    expect(responseBody.total).toBe(12);
+    expect(responseBody.total_pages).toBe(2);
+
+    // Assert data array
+    expect(responseBody.data.length).toBe(responseBody.per_page);
+    (responseBody.data as User[]).forEach((user) => {
+      expect(user).toHaveProperty('id');
+      expect(user).toHaveProperty('email');
+      expect(user).toHaveProperty('first_name');
+      expect(user).toHaveProperty('last_name');
+      expect(user).toHaveProperty('avatar');
+    });
+
+    // Assert first user
+    const firstUser = responseBody.data[0];
+    expect(firstUser.id).toBe(1);
+    expect(firstUser.email).toBe('george.bluth@reqres.in');
+    expect(firstUser.first_name).toBe('George');
+    expect(firstUser.last_name).toBe('Bluth');
+    expect(firstUser.avatar).toBe('https://reqres.in/img/faces/1-image.jpg');
+
+    // Assert second user
+    const secondUser = responseBody.data[1];
+    expect(secondUser.id).toBe(2);
+    expect(secondUser.email).toBe('janet.weaver@reqres.in');
+    expect(secondUser.first_name).toBe('Janet');
+    expect(secondUser.last_name).toBe('Weaver');
+    expect(secondUser.avatar).toBe('https://reqres.in/img/faces/2-image.jpg');
+  });
+
+  test('Assert Response for user from page 2', async ({ request }) => {
+    const response = await request.get(`${baseUrl}/users`, {
+      params: { page: 2 },
+    });
+
+    // Assert
+    expect(response.status()).toBe(200);
+    console.log('Status:', response.status());
+
+    // Response body
+    const responseBody = JSON.parse(await response.text());
+    console.log(responseBody);
+
+    // Assert structure
+    expect(responseBody).toHaveProperty('page');
+    expect(responseBody).toHaveProperty('per_page');
+    expect(responseBody).toHaveProperty('total');
+    expect(responseBody).toHaveProperty('total_pages');
+    expect(responseBody).toHaveProperty('data');
+
+    // Assert values
+    expect(responseBody.page).toBe(2);
+    expect(responseBody.per_page).toBe(6);
+    expect(responseBody.total).toBe(12);
+    expect(responseBody.total_pages).toBe(2);
+
+    // Assert data array
+    expect(responseBody.data.length).toBe(responseBody.per_page);
+    (responseBody.data as User[]).forEach((user) => {
+      expect(user).toHaveProperty('id');
+      expect(user).toHaveProperty('email');
+      expect(user).toHaveProperty('first_name');
+      expect(user).toHaveProperty('last_name');
+      expect(user).toHaveProperty('avatar');
+    });
+
+    // Assert first user
+    const firstUser = responseBody.data[0];
+    expect(firstUser.id).toBe(7);
+    expect(firstUser.email).toBe('michael.lawson@reqres.in');
+    expect(firstUser.first_name).toBe('Michael');
+    expect(firstUser.last_name).toBe('Lawson');
+    expect(firstUser.avatar).toBe('https://reqres.in/img/faces/7-image.jpg');
+
+    // Assert second user
+    const secondUser = responseBody.data[1];
+    expect(secondUser.id).toBe(8);
+    expect(secondUser.email).toBe('lindsay.ferguson@reqres.in');
+    expect(secondUser.first_name).toBe('Lindsay');
+    expect(secondUser.last_name).toBe('Ferguson');
+    expect(secondUser.avatar).toBe('https://reqres.in/img/faces/8-image.jpg');
+  });
+});
+
+
+/*
+Response body:
+
+{
+  page: 2,
+  per_page: 6,
+  total: 12,
+  total_pages: 2,
+  data: [
+    {
+      id: 7,
+      email: 'michael.lawson@reqres.in',
+      first_name: 'Michael',
+      last_name: 'Lawson',
+      avatar: 'https://reqres.in/img/faces/7-image.jpg'
+    },
+    {
+      id: 8,
+      email: 'lindsay.ferguson@reqres.in',
+      first_name: 'Lindsay',
+      last_name: 'Ferguson',
+      avatar: 'https://reqres.in/img/faces/8-image.jpg'
+    },
+    {
+      id: 9,
+      email: 'tobias.funke@reqres.in',
+      first_name: 'Tobias',
+      last_name: 'Funke',
+      avatar: 'https://reqres.in/img/faces/9-image.jpg'
+    },
+    {
+      id: 10,
+      email: 'byron.fields@reqres.in',
+      first_name: 'Byron',
+      last_name: 'Fields',
+      avatar: 'https://reqres.in/img/faces/10-image.jpg'
+    },
+    {
+      id: 11,
+      email: 'george.edwards@reqres.in',
+      first_name: 'George',
+      last_name: 'Edwards',
+      avatar: 'https://reqres.in/img/faces/11-image.jpg'
+    },
+    {
+      id: 12,
+      email: 'rachel.howell@reqres.in',
+      first_name: 'Rachel',
+      last_name: 'Howell',
+      avatar: 'https://reqres.in/img/faces/12-image.jpg'
+    }
+  ],
+  support: {
+    url: 'https://contentcaddy.io?utm_source=reqres&utm_medium=json&utm_campaign=referral',
+    text: 'Tired of writing endless social media content? Let Content Caddy generate it for you.'
+  }
+}
+
+*/
