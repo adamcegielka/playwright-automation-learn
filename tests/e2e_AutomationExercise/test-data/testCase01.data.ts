e12a417257f487723fb19dc45235a9eb6f750956
Adam Cegiełka <adamcegielka@tlen.pl>
2025-05-04 10:41:48 +0200

    feat: add test data

---
+++ tests/e2e_AutomationExercise/test-data/testCase01.data.ts
@@ -0,0 +1,7 @@
+export const testCase01Data = {
+    verifyHomePage: 'Automation Exercise',
+    verifyNewUser: 'New User Signup!',
+    verifEenterAccountInformation: 'ENTER ACCOUNT INFORMATION',
+    verifAccountCreated: 'ACCOUNT CREATED!',
+    verifAccountDeleted: 'ACCOUNT DELETED!',
+  };
