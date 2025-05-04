1dfd114c12c7280acde0e9a5321ff462647a77cf
Adam Cegiełka <adamcegielka@tlen.pl>
2025-05-04 11:21:16 +0200

    refactor

--- tests/e2e_AutomationExercise/page-objects/HomePage.ts
+++ tests/e2e_AutomationExercise/page-objects/HomePage.ts
@@ -2,19 +2,19 @@ import { expect, Page } from '@playwright/test';
 
 export class HomePage {
   readonly page: Page;
-  readonly urlCart: Page;
 
   constructor(page: Page) {
     this.page = page;
-    this.urlCart = page;
   }
 
   async navHomePage() {
     await this.page.goto('https://automationexercise.com/');
+    await this.page.getByRole('button', { name: 'Consent' }).click();
+    // await this.page.getByRole('button', { name: 'Zgadzam się' }).click(); // for Polish
   }
 
   // Assertions
-  async verifytTitlePage() {
+  async verifyTitlePage() {
     await expect(this.page).toHaveTitle('Automation Exercise');
   }
 
@@ -23,8 +23,6 @@ export class HomePage {
   }
 
   async verifyUrlCart() {
-    await expect(this.page).toHaveURL(
-      'https://automationexercise.com/view_cart'
-    );
+    await expect(this.page).toHaveURL('https://automationexercise.com/view_cart');
   }
 }
