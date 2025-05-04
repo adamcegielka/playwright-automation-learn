00210dd4a687635e2ee39841f82686f3e91fe1a7
Adam Cegiełka <adamcegielka@tlen.pl>
2025-05-04 10:42:42 +0200

    feat: add page object HomePage

---
+++ tests/e2e_AutomationExercise/page-objects/HomePage.ts
@@ -0,0 +1,30 @@
+import { expect, Page } from '@playwright/test';
+
+export class HomePage {
+  readonly page: Page;
+  readonly urlCart: Page;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.urlCart = page;
+  }
+
+  async navHomePage() {
+    await this.page.goto('https://automationexercise.com/');
+  }
+
+  // Assertions
+  async verifytTitlePage() {
+    await expect(this.page).toHaveTitle('Automation Exercise');
+  }
+
+  async verifyHomePage() {
+    await expect(this.page).toHaveURL('https://automationexercise.com/');
+  }
+
+  async verifyUrlCart() {
+    await expect(this.page).toHaveURL(
+      'https://automationexercise.com/view_cart'
+    );
+  }
+}
