9808f77824f8def72fa5e0e49aaa4ca96db764e2
Adam Cegiełka <adamcegielka@tlen.pl>
2025-05-05 19:34:51 +0200

    feat: add test data for test case 03

---
+++ tests/e2e_AutomationExercise/test-data/testCase03.data.ts
@@ -0,0 +1,7 @@
+export const testCase03Data = {
+  incorrectUserEmail: 'new-incorrect@mail.com',
+  incorrectUserPassword: 'incorrect234',
+  verifyHomePage: 'Automation Exercise',
+  verifyLoginToAccount: 'Login to your account',
+  verifyErrorMessage: 'Your email or password is incorrect!',
+};
