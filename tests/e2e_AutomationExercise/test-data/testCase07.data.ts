4a8d89905ad5c883e115eba6a62b608207774076
Adam Cegiełka <adamcegielka@tlen.pl>
2025-05-07 20:19:44 +0200

    feat: add test data for test case 07

---
+++ tests/e2e_AutomationExercise/test-data/testCase07.data.ts
@@ -0,0 +1,6 @@
+export const testCase07Data = {
+  verifyHomePage: 'Automation Exercise',
+  verifyTestCasesPage: 'https://automationexercise.com/test_cases',
+  verifyTestCasesPageTitle:
+    'Automation Practice Website for UI Testing - Test Cases',
+};
