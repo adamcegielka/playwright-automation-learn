c72a9f3f3b258f0329892cf4a62b83dae43b480c
Adam Cegiełka <adamcegielka@tlen.pl>
2025-05-10 14:53:28 +0200

    feat: add test data  test payment by card

---
+++ tests/e2e_AutomationExercise/test-data/testPaymentByCard.data.ts
@@ -0,0 +1,7 @@
+export const testPaymentByCarsData = {
+  cardVendor: 'Visa',
+  cardNumber: '4070723055941987',
+  cardCvv: '981',
+  cardExpirationDateMonth: '11',
+  cardExpirationDateYear: '25',
+};
