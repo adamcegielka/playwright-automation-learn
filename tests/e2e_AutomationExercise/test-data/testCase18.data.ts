c7f26f91f672888d8dfe344424376a834213e02c
Adam Cegiełka <adamcegielka@tlen.pl>
2025-05-11 13:21:10 +0200

    feat: add test data for test case 18

---
+++ tests/e2e_AutomationExercise/test-data/testCase18.data.ts
@@ -0,0 +1,6 @@
+export const testCase18Data = {
+  verifyCategories:
+    'Category Women Dress Tops Saree Men Tshirts Jeans Kids Dress Tops & Shirts Brand',
+  verifyCategoryWomen: 'Women - Tops Products',
+  verifyCategoryMen: 'Men - Jeans Products',
+};
