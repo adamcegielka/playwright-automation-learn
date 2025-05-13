3bd6771c335bbd423c030846e91826d8095d47e7
Adam Cegiełka <adamcegielka@tlen.pl>
2025-05-13 20:20:30 +0200

    feat: add test data for test case 21

---
+++ tests/e2e_AutomationExercise/test-data/testCase21.data.ts
@@ -0,0 +1,8 @@
+export const testCase21Data = {
+  userEmail: 'adrian.jackson@mail.com',
+  nameSurname: 'Adrian Jackson',
+  review: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  verifyProducts: 'All Products',
+  verifyProductsAll: 'All Products  Added! Your product has been added to cart. View Cart Continue Sh',
+  verifySuccessMessage: 'Thank you for your review.',
+};
