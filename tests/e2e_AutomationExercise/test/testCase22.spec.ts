21c2b7808771155855ed8192697a8b6dd45256f2
Adam Cegiełka <adamcegielka@tlen.pl>
2025-05-14 15:19:51 +0200

    feat: add test case 22

---
+++ tests/e2e_AutomationExercise/test/testCase22.spec.ts
@@ -0,0 +1,42 @@
+import { test, expect, chromium } from '@playwright/test';
+import { testCase22Data } from '../test-data/testCase22.data';
+import { HomePage } from '../page-objects/HomePage';
+
+test.describe('Test Case 22: Add to cart from Recommended items', () => {
+  test('TC22 POM add to cart from recommended items', async ({ page }) => {
+    const homePage = new HomePage(page);
+
+    const verifyRecommendedItems = testCase22Data.verifyRecommendedItems;
+    const verifyProductIsDisplays = testCase22Data.verifyProductIsDisplays;
+
+    // Blocking of network resources that generate Ads
+    await page.route('**/*', (route) => {
+      route.request().url().startsWith('https://googleads.') ? route.abort() : route.continue();
+      return;
+    });
+    // --- End code
+
+    await chromium.launch();
+    await homePage.navHomePage();
+    await page.evaluate(() => {
+      window.scrollTo(0, document.body.scrollHeight);
+    });
+    await expect(await page.getByRole('heading', { name: verifyRecommendedItems })).toBeVisible();
+    await page
+      .locator('div:nth-child(2) > .product-image-wrapper > .single-products > .productinfo > .add-to-cart')
+      .first()
+      .click();
+    await page.getByRole('link', { name: 'View Cart' }).click();
+    await expect(page.getByRole('row', { name: verifyProductIsDisplays })).toBeVisible();
+  });
+});
+
+/*
+1. Launch browser
+2. Navigate to url 'http://automationexercise.com'
+3. Scroll to bottom of page
+4. Verify 'RECOMMENDED ITEMS' are visible
+5. Click on 'Add To Cart' on Recommended product
+6. Click on 'View Cart' button
+7. Verify that product is displayed in cart page
+*/
