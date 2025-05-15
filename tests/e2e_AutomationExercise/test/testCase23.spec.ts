9c07697dea30d68fdb8e06d67770e180d0039ca9
Adam Cegiełka <adamcegielka@tlen.pl>
2025-05-15 19:25:54 +0200

    feat: add test case 23

---
+++ tests/e2e_AutomationExercise/test/testCase23.spec.ts
@@ -0,0 +1,69 @@
+import { test, expect, chromium } from '@playwright/test';
+import { testCase23Data } from '../test-data/testCase23.data';
+import { testRegistrationData } from '../test-data/testRegistration.data';
+import { HomePage } from '../page-objects/HomePage';
+import { Navbar } from '../page-objects/components/Navbar';
+import { RegistrationUser } from '../page-objects/RegistrationUser';
+import { CartPage } from '../page-objects/CartPage';
+import { DeletionUser } from '../page-objects/DeletionUser';
+
+test.describe('Test Case 23: Verify address details in checkout page', () => {
+  test('TC23 POM verify address details in checkout page', async ({ page }) => {
+    const homePage = new HomePage(page);
+    const navbar = new Navbar(page);
+    const registrationUser = new RegistrationUser(page);
+    const cartPage = new CartPage(page);
+    const deletionUser = new DeletionUser(page);
+
+    const userId = testRegistrationData.userId;
+    const urlCart = testCase23Data.urlCart;
+    const verifyShoppingCart = testCase23Data.verifyShoppingCart;
+
+    // Blocking of network resources that generate Ads
+    await page.route('**/*', (route) => {
+      route.request().url().startsWith('https://googleads.') ? route.abort() : route.continue();
+      return;
+    });
+    // --- End code
+
+    await chromium.launch();
+    await homePage.navHomePage();
+    await homePage.verifyHomePage();
+    await homePage.verifyTitlePage();
+    await navbar.clickOnNav('Signup / Login');
+    await registrationUser.enterNameEmail();
+    await registrationUser.createAccount();
+    await registrationUser.messageAccountCreated();
+    await registrationUser.clickButton();
+    await expect(page.getByText(`Logged in as ${userId}`)).toBeVisible();
+    const winterTop = await page.waitForSelector('[data-product-id="5"]');
+    await winterTop.click();
+    await page.getByRole('link', { name: 'View Cart' }).click();
+    await expect(page).toHaveURL(urlCart);
+    await expect(page.getByText(verifyShoppingCart)).toBeVisible();
+    await cartPage.clickbuttonProToCheckout();
+    await cartPage.assertAddressDelivery();
+    await cartPage.assertAddressBillings();
+    await deletionUser.clickDeleteButton();
+    await deletionUser.messageAccountDeleted();
+    await deletionUser.clickContinueButton();
+  });
+});
+
+/*
+1. Launch browser
+2. Navigate to url 'http://automationexercise.com'
+3. Verify that home page is visible successfully
+4. Click 'Signup / Login' button
+5. Fill all details in Signup and create account
+6. Verify 'ACCOUNT CREATED!' and click 'Continue' button
+7. Verify ' Logged in as username' at top
+8. Add products to cart
+9. Click 'Cart' button
+10. Verify that cart page is displayed
+11. Click Proceed To Checkout
+12. Verify that the delivery address is same address filled at the time registration of account
+13. Verify that the billing address is same address filled at the time registration of account
+14. Click 'Delete Account' button
+15. Verify 'ACCOUNT DELETED!' and click 'Continue' button
+*/
