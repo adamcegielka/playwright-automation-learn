2d6ba4a0d7ad99aafdb03feafd6d1fa54d9c5bbb
Adam Cegiełka <adamcegielka@tlen.pl>
2025-05-17 11:16:53 +0200

    feat: add test case 26

---
+++ tests/e2e_AutomationExercise/test-data/testCase26.data.ts
@@ -0,0 +1,5 @@
+export const testCase26Data = {
+  verifyHomePage: 'Automation Exercise',
+  verifySubscription: 'Subscription',
+  verifyVisibleScreen: 'Full-Fledged practice website for Automation Engineers',
+};
